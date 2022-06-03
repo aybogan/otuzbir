66fe1789964cd6aa1a453c100edb7fdb9b77c85b
ayıboğan <45512651+aybogan@users.noreply.github.com>
2022-06-03 17:40:21 +0300

    Update otuzbir.py

--- otuzbir.py
+++ otuzbir.py
@@ -1,1 +1,16 @@
+import strign
+from random import *
+
+def otuzbir():
+  a = input("Bir sayı gir: ") 
+  if a == ("31"):
+    while True:
+      karakter = string.ascii_letters
+      random = "".join(choise(karakter) for i in range(randit(31,69)) 
+      print(random.upper()) 
+  while a != ("31") :
+     print("GÜLEMEM KARDEŞ 3'ÜN YANINDA 1'İ GÖRMEDEN GÜLEMEM") 
+     otuzbir() 
+
+otuzbir() 
 
